340450dd5cdc66d33b7f71b377f102cf3d0b16f1
Lgrion <82838993+lucianogrion@users.noreply.github.com>
2025-02-07 15:00:14 -0300

    Add files via upload

---
+++ EjemploBursatil/Models/DataPointOHLC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace EjemploBursatil.Models
+{
+    [DataContract]
+    public class DataPointOHLC
+    {
+        public DataPointOHLC(long x, double[] y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "x")]
+        public long X;
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public double[] Y = null;
+    }
+}
